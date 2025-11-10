b97af70f25d0c3be58b00361c6b2016ae2e3bd64
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-10 13:51:39 -0500

    added H1 styled component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,9 @@
-"use client";
-
-import { motion } from "framer-motion";
-import { generateHexCode } from "./lib/generateHexCode";
+import H1 from "./components/H1";
 
 export default function Home() {
-  const color = generateHexCode();
-  console.log(color);
-
   return (
     <main className="flex flex-col justify-center items-center">
-      <motion.h1
-        animate={{ scale: [1, 1.05, 1] }} // pulse effect
-        transition={{
-          duration: 1, // 1 second for one pulse
-          repeat: Infinity, // loop forever
-          repeatType: "loop",
-          ease: "easeInOut", // smooth animation
-        }}
-        className="text-5xl"
-      >
-        Color Palette Creator
-      </motion.h1>
+      <H1>Color Palette Creator</H1>
     </main>
   );
 }
