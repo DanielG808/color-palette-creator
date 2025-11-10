d75a59c15536933e4de12e180d5f6cff9da7cae0
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-10 16:55:33 -0500

    rendered random hex codes in color-chips; x button now removes the color-chip it's rendered on

--- components/color-chip.tsx
+++ components/color-chip.tsx
@@ -1,20 +1,32 @@
 import { XIcon } from "lucide-react";
 
 type ColorChipProps = {
-  numColors: number;
+  color: string;
+  colorsLength: number;
+  onRemove: () => void;
 };
 
-export default function ColorChip({ numColors }: ColorChipProps) {
+export default function ColorChip({
+  color,
+  colorsLength,
+  onRemove,
+}: ColorChipProps) {
   return (
     <div className="group relative inline-block">
-      {numColors > 3 && (
-        <button className="absolute top-0 -right-6 p-1 text-calm-5/75 hover:text-calm-5 cursor-pointer opacity-0 group-hover:opacity-100 duration-200">
+      {colorsLength > 3 && (
+        <button
+          onClick={onRemove}
+          className="absolute top-0 -right-6 p-1 text-calm-5/75 hover:text-calm-5 cursor-pointer opacity-0 group-hover:opacity-100 duration-200"
+        >
           <XIcon />
         </button>
       )}
 
       <div className="flex justify-center items-center border border-calm-4/75 rounded-full w-24 h-24 shadow-lg hover:shadow-2xl duration-200">
-        <div className="bg-[#F54927] rounded-full w-20 h-20" />
+        <div
+          className="rounded-full w-20 h-20"
+          style={{ backgroundColor: color }}
+        />
       </div>
     </div>
   );
