b15625af3afb455cc053bfd1ee9d845ad548cf20
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-10 15:09:56 -0500

    added colors to globals.css

--- components/color-palette-canvas.tsx
+++ components/color-palette-canvas.tsx
@@ -1,3 +1,7 @@
 export default function ColorPaletteCanvas() {
-  return <div>ColorPaletteCanvas</div>;
+  return (
+    <section className="border border-calm-3/75 px-4 py-2 rounded-md">
+      ColorPaletteCanvas
+    </section>
+  );
 }
