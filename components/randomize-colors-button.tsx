f8b3e7444daac172c597982959c7f5de61d8d492
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-10 17:08:04 -0500

    added randomize-colors-button to color-palette-canvas

--- components/randomize-colors-button.tsx
+++ components/randomize-colors-button.tsx
@@ -1,3 +1,7 @@
 export default function RandomizeColorsButton() {
-  return <button>Randomize Colors</button>;
+  return (
+    <button className="flex justify-center items-center p-3 mt-10 rounded-md bg-calm-3/75 hover:bg-calm-3 cursor-pointer duration-200">
+      Randomize Colors
+    </button>
+  );
 }
