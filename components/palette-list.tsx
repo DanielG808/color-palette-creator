d4fa38b62ae256a4c22d5e8af97ab53995e2d91c
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-12 08:23:40 -0500

    fixed ui bug; too much space on right side of palette-list

--- components/palette-list.tsx
+++ components/palette-list.tsx
@@ -12,15 +12,21 @@ export default function PaletteList({
   deletePalette,
 }: PaletteListProps) {
   return (
-    <ul className="space-y-1 overflow-y-auto max-h-full pr-4">
-      {palettes.map((palette, index) => (
-        <PaletteChip
-          key={palette.id}
-          index={index}
-          palette={palette}
-          deletePalette={deletePalette}
-        />
-      ))}
+    <ul className="space-y-1 overflow-y-auto max-h-full">
+      {palettes.length === 0 ? (
+        <p className="text- text-calm-3/75">
+          Save a palette and see it displayed here...
+        </p>
+      ) : (
+        palettes.map((palette, index) => (
+          <PaletteChip
+            key={palette.id}
+            index={index}
+            palette={palette}
+            deletePalette={deletePalette}
+          />
+        ))
+      )}
     </ul>
   );
 }
