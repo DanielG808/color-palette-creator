cab6252b06f4eed54542333b6b045b12274ca8b8
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-12 11:44:25 -0500

    made dark mode OS sensitive and toggle-able

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${momoTrustDisplay.className} flex flex-col items-center min-h-screen p-10 antialiased`}
       >
--- components/home-page-link.tsx
+++ components/home-page-link.tsx
@@ -3,7 +3,7 @@ import H1 from "@/components/H1";
 export default function HomePageLink() {
   return (
     <a href="/">
-      <H1 className="hover:text-black duration-200 cursor-pointer">
+      <H1 className="hover:text-black dark:hover:text-white duration-200 cursor-pointer">
         Color Palette Creator
       </H1>
     </a>
