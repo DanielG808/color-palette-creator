833eab538b27ea91e49b0499f6705d573023c0e8
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-12 14:42:43 -0500

    added dark mode colors to plus-button

--- components/plus-button.tsx
+++ components/plus-button.tsx
@@ -8,7 +8,7 @@ export default function PlusButton({ onClick }: PlusButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="flex justify-center items-center w-24 h-24 rounded-full text-calm-5/75 bg-calm-2/50 hover:text-calm-5 hover:bg-calm-2 cursor-pointer duration-200"
+      className="flex justify-center items-center w-24 h-24 rounded-full text-calm-5/75 dark:text-white/75 bg-calm-2/50 dark:bg-white/25 hover:text-calm-5 dark:hover:text-white hover:bg-calm-2 dark:hover:bg-white/45 cursor-pointer duration-200"
     >
       <PlusIcon />
     </button>
