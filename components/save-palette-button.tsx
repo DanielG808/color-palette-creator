dfc27fcb815fcbe0ab02e01870363bc5d25d3449
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-12 14:30:07 -0500

    added dark mode styling to save-palette-button

--- components/save-palette-button.tsx
+++ components/save-palette-button.tsx
@@ -8,7 +8,7 @@ export default function SavePaletteButton({
   return (
     <button
       onClick={savePalette}
-      className="flex justify-center items-center p-3 mt-3 rounded-md bg-calm-3/75 hover:bg-calm-3 cursor-pointer duration-200 disabled:cursor-not-allowed disabled:bg-calm-2/50 disabled:text-calm-5/50"
+      className="flex justify-center items-center p-3 mt-3 rounded-md bg-calm-3/75 dark:bg-white/25 hover:bg-calm-3 dark:hover:bg-white/35 cursor-pointer duration-200 disabled:cursor-not-allowed disabled:bg-calm-2/50 disabled:text-calm-5/50"
     >
       Save Palette
     </button>
