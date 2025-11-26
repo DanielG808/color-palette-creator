87835c4572b0ee7e9ab15fef86055e8aa2d7ff34
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-26 13:33:09 -0500

    inverted icons on mode toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -32,7 +32,7 @@ export default function ModeToggle() {
       }}
       aria-label="Toggle theme"
     >
-      {theme === "dark" ? <Moon /> : <Sun />}
+      {theme === "dark" ? <Sun /> : <Moon />}
     </button>
   );
 }
