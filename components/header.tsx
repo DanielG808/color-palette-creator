141c0643c4c6b402bf9fb6fcb2fd579c7314f2ee
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-26 11:32:46 -0500

    added layout styling to header

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import HomePageLink from "./home-page-link";
 
 export default function Header() {
   return (
-    <header className="flex justify-center">
+    <header className="flex justify-center items-center space-x-5">
       <HomePageLink />
       <ModeToggle />
     </header>
