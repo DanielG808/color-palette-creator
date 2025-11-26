1fbc12986b5a12117a639697acfceff0de96e761
Dan Gray <daniel.w.gray1994@gmail.com>
2025-11-26 13:36:29 -0500

    added mobile layout styling to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-auto text-xs">
+    <footer className="mt-auto text-xs text-center pt-8 md:pt-0">
       &copy;{new Date().getFullYear()} Color Palette Creator. All rights
       reserved.
     </footer>
